1fdce205f3f040af62abe0a9d1f9780842d25e07
Christian <c.wolfram@chriwo.de>
2023-10-27 09:55:46 +0000

    [BUGFIX][#123] Respect flexform, categories and default settings for news link generation

--- Classes/DataProcessing/Records/LinkProcessorService.php
+++ Classes/DataProcessing/Records/LinkProcessorService.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace StarterTeam\StarterTwigNews\DataProcessing\Records;
 
+use GeorgRinger\News\Domain\Model\Category;
 use GeorgRinger\News\Domain\Model\News;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 class LinkProcessorService implements NewsProcessorInterface
 {
+    protected array $detailPidDeterminationCallbacks = [
+        'flexform' => 'getDetailPidFromFlexForm',
+        'categories' => 'getDetailPidFromCategories',
+        'default' => 'getDetailPidFromDefaultDetailPid',
+    ];
+
     private ContentObjectRenderer $contentObjectRenderer;
 
     public function __construct(ContentObjectRenderer $contentObjectRenderer)
@@ -32,7 +40,7 @@ class LinkProcessorService implements NewsProcessorInterface
             'title' => $newsRecord->getTitle(),
         ];
 
-        $typeLinkConfig = $this->getLinkConfigurationByNewsType($newsRecord, (int)$configuration['detailPid']);
+        $typeLinkConfig = $this->getLinkConfigurationByNewsType($newsRecord, $configuration);
         $typoLinkConfig = array_merge($typeLinkConfig, $basicLinkConfig);
         $uri = $this->contentObjectRenderer->typoLink_URL($typoLinkConfig);
         if ($uri === '') {
@@ -49,7 +57,7 @@ class LinkProcessorService implements NewsProcessorInterface
         ];
     }
 
-    private function getLinkConfigurationByNewsType(News $newsRecord, int $defaultDetailPid): array
+    private function getLinkConfigurationByNewsType(News $newsRecord, array $configuration): array
     {
         switch (NewsProcessorService::NEWS_TYPE[$newsRecord->getType()]) {
             case 'internal':
@@ -65,20 +73,84 @@ class LinkProcessorService implements NewsProcessorInterface
                 ];
                 break;
             default:
-                $typoLinkConfig = [
-                    'parameter' => $defaultDetailPid,
-                    'target' => '_self',
-                    'additionalParams' => '&' . GeneralUtility::implodeArrayForUrl(
-                        'tx_news_pi1',
-                        [
-                            'news' => $newsRecord->getUid(),
-                            'controller' => 'News',
-                            'action' => 'detail',
-                        ]
-                    ),
-                ];
+                $typoLinkConfig = $this->getLinkToNewsItem($newsRecord, $configuration);
         }
 
         return $typoLinkConfig;
+    }
+
+    protected function getLinkToNewsItem(News $newsRecord, array $configuration): array
+    {
+        $typoLinkParameter = isset($configuration['detailPid']) ? (int)$configuration['detailPid'] : 0;
+
+        if ($typoLinkParameter === 0) {
+            if (!isset($configuration['detailPidDetermination'])) {
+                $detailPidDeterminationMethods = ['flexform'];
+            } else {
+                $detailPidDeterminationMethods = GeneralUtility::trimExplode(
+                    ',',
+                    $configuration['detailPidDetermination'],
+                    true
+                );
+            }
+
+            foreach ($detailPidDeterminationMethods as $determinationMethod) {
+                if ($callback = $this->detailPidDeterminationCallbacks[$determinationMethod]) {
+                    $typoLinkParameter = call_user_func($callback, $newsRecord, $configuration);
+                    if (is_int($typoLinkParameter)) {
+                        break;
+                    }
+                }
+            }
+
+            if (!$typoLinkParameter && isset($GLOBALS['TSFE'])) {
+                $typoLinkParameter = $GLOBALS['TSFE']->id;
+            }
+        }
+
+        return [
+            'parameter' => $typoLinkParameter,
+            'target' => '_self',
+            'additionalParams' => '&' . GeneralUtility::implodeArrayForUrl(
+                'tx_news_pi1',
+                [
+                    'news' => $newsRecord->getUid(),
+                    'controller' => 'News',
+                    'action' => 'detail',
+                ]
+            ),
+        ];
+    }
+
+    /**
+     * Gets detailPid from categories of the given news item. First will be return.
+     */
+    protected function getDetailPidFromCategories(News $newsRecord, array $configuration): int
+    {
+        $detailPid = 0;
+        if ($newsRecord->getCategories() instanceof ObjectStorage) {
+            foreach ($newsRecord->getCategories() as $category) {
+                if ($category instanceof Category && ($detailPid = $category->getSinglePid())) {
+                    break;
+                }
+            }
+        }
+        return $detailPid;
+    }
+
+    /**
+     * Gets detailPid from defaultDetailPid setting
+     */
+    protected function getDetailPidFromDefaultDetailPid(News $newsItem, array $configuration): int
+    {
+        return isset($configuration['defaultDetailPid']) ? (int)$configuration['defaultDetailPid'] : 0;
+    }
+
+    /**
+     * Gets detailPid from flexform of current plugin.
+     */
+    protected function getDetailPidFromFlexForm(News $newsItem, array $configuration): int
+    {
+        return isset($configuration['detailPid']) ? (int)$configuration['detailPid'] : 0;
     }
 }
